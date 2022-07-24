e19d4e3f85f5199cf2ea87bf9c055799d6b97520
kj35 <olivewhitdev@gmail.com>
2022-07-24 15:14:44 -0400

    add delete functionality

--- public/main.js
+++ public/main.js
@@ -18,3 +18,29 @@ const enterButton = document.querySelector("#enterButton");
         window.location.reload(true);
     })
 })*/
+
+const deleteButtons = document.querySelectorAll("img");
+let taskId = "";
+
+for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener('click', e => {
+        const li = e.target.parentNode
+        console.log(li.id);
+        taskId = "" + li.id;
+        fetch(`/deleteTask/${taskId}`, {
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                id: taskId,
+                isComplete: false
+            })
+        })
+        .then(res => {
+            if( res.ok) return res.json()
+        })
+        .then(res => window.location.reload())
+        .catch(data => {
+            window.location.reload()
+        })
+    })
+}
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-
+const { ObjectId } = require('mongodb')
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todolist',
     collection
 
-const MongoClient =  require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect(dbConnectionStr)
     .then(client => {
         console.log("connected to database");
@@ -19,7 +19,7 @@ MongoClient.connect(dbConnectionStr)
         collection = db.collection('tasks');
     })
 
-    
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -30,9 +30,9 @@ app.get("/", (request, response) => {
     //find all tasks
     collection.find().toArray()
         .then(results => {
-            console.log(results);
+            //console.log(results);
             //send it to the ejs file
-            response.render('index.ejs', { tasks: results})
+            response.render('index.ejs', { tasks: results })
         })
         .catch(error => console.log(error))
 })
@@ -43,12 +43,25 @@ app.post('/addtask', (req, res) => {
         name: req.body.name,
         isComplete: false
     })
-      .then(result => {
-        console.log(result)
-        res.redirect("/")
-        
-      })
-      .catch(error => console.error(error))
+        .then(result => {
+            //console.log(result)
+            res.redirect("/")
+
+        })
+        .catch(error => console.error(error))
+})
+
+//deletebutton
+app.delete('/deletetask/:id', (req, res) => {
+    console.log(req.params.id);
+    collection.deleteOne({
+        "_id": ObjectId(req.params.id)
+    })
+        .then(result => {
+            console.log(result)
+            res.json("Deleted by id")
+        })
+        .catch(error => console.log(error))
 })
 
 const port = process.env.PORT || 3000
